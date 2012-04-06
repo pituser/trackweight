f3db11f1546236f99fc9de1b058a87be11d41342
Mike Lerch <mike.lerch@gmx.de>
2012-04-06 11:15:08 +0200

    Fixed home page test after renaming app.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home page" do
-    it "should have the content 'Rails Test'" do
+    it "should have the content 'TrackWeight'" do
       visit '/'
-      page.should have_content('Rails Test')
+      page.should have_content('TrackWeight')
     end
   end
   
